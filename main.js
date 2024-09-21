2f549223bdce528d7468b96d5c6f9d58b86bc1b1
Mo-deck <mohamedabow6774@gmail.com>
2024-09-22 00:24:20 +0300

    advanced username

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const form = document.querySelector('#registrationForm')
 
 form.addEventListener("submit", function(event){
     event.preventDefault()
+
+    // form.addEventListener("submit", function(event){
+//     event.preventDefault()
     
     const username = document.querySelector("#username").value;
     const email = document.querySelector("#email").value;
@@ -10,28 +13,53 @@ form.addEventListener("submit", function(event){
     
     const error = document.querySelector('#error')
 
-        //username checking
-        if(username === ''){
-            error.textContent = "username is required"
-            return //codekan meshan ku jooji 
-        }
+//         //username checking
+//         if(username === ''){
+//             error.textContent = "username is required"
+//             return //codekan meshan ku jooji 
+//         }
     
-            //Email checking
+//             //Email checking
+//     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+//     if(!email.match(emailPattern)){
+//         error.textContent = "Please enter a valid eamil address"
+//         return;
+//     }
+//   //Password
+//     if(password.length < 8){
+//         error.textContent = "Password must be at least 8 characters long."
+//         return
+//     }
+//     //Confirm Password
+//     if(password !== consfirmPassword){
+//         error.textContent = "password do not match"
+//         return
+//     }
+
+   
+  //username checking
+  if(username === ''){
+    error.textContent = "username is required"
+     return //codekan meshan ku jooji 
+  }
+
+      //Email checking
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if(!email.match(emailPattern)){
-        error.textContent = "Please enter a valid eamil address"
+         error.textContent = "Please enter a valid eamil address"
         return;
     }
-  //Password
-    if(password.length < 8){
-        error.textContent = "Password must be at least 8 characters long."
+
+        //Password
+     if(password.length < 8){
+         error.textContent = "Password must be at least 8 characters long."
         return
-    }
-    //Confirm Password
-    if(password !== consfirmPassword){
-        error.textContent = "password do not match"
-        return
-    }
+     }
+     //Confirm Password
+     if(password !== consfirmPassword){
+         error.textContent = "password do not match"
+         return
+     }
 
     error.textContent = "Registered successfully"
 })
