10ed5711ffb5246f03b67f1b09396f6a74773d47
Mo-deck <mohamedabow6774@gmail.com>
2024-09-22 08:21:22 +0300

    another check username

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const form = document.querySelector('#registrationForm')
 
-form.addEventListener("submit", function(event){
-    event.preventDefault()
+//form.addEventListener("submit", function(event){
+// event.preventDefault()
 
     // form.addEventListener("submit", function(event){
 //     event.preventDefault()
     
-    const username = document.querySelector("#username").value;
-    const email = document.querySelector("#email").value;
-    const password = document.querySelector("#password").value;
-    const consfirmPassword = document.querySelector("#confirmPassword").value;
+    // const username = document.querySelector("#username").value;
+    // const email = document.querySelector("#email").value;
+    // const password = document.querySelector("#password").value;
+    // const consfirmPassword = document.querySelector("#confirmPassword").value;
     
-    const error = document.querySelector('#error')
+    // const error = document.querySelector('#error')
 
 //         //username checking
 //         if(username === ''){
@@ -38,28 +38,68 @@ form.addEventListener("submit", function(event){
 
    
   //username checking
-  if(username === ''){
-    error.textContent = "username is required"
-     return //codekan meshan ku jooji 
-  }
+//   if(username === ''){
+//     error.textContent = "username is required"
+//      return //codekan meshan ku jooji 
+//   }
 
-      //Email checking
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if(!email.match(emailPattern)){
-         error.textContent = "Please enter a valid eamil address"
-        return;
-    }
+//       //Email checking
+//     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+//     if(!email.match(emailPattern)){
+//          error.textContent = "Please enter a valid eamil address"
+//         return;
+//     }
 
-        //Password
-     if(password.length < 8){
-         error.textContent = "Password must be at least 8 characters long."
-        return
-     }
-     //Confirm Password
-     if(password !== consfirmPassword){
-         error.textContent = "password do not match"
-         return
-     }
+//         //Password
+//      if(password.length < 8){
+//          error.textContent = "Password must be at least 8 characters long."
+//         return
+//      }
+//      //Confirm Password
+//      if(password !== consfirmPassword){
+//          error.textContent = "password do not match"
+//          return
+//      }
 
-    error.textContent = "Registered successfully"
+//     error.textContent = "Registered successfully"
+//})
+
+const username = document.querySelector("#username")
+const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+const consfirmPassword = document.querySelector("#confirmPassword")
+const error = document.querySelector('#error')
+const success = document.querySelector('#success')
+
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+   //validate fields
+   const isUsernameValid = validateUsername()
 })
+
+ //check username
+ function validateUsername(){
+    if(username.value === ''){
+        //error
+        SetError(username, 'Username is required')
+        return false
+    }else{
+        //success
+        setSuccess(username);
+        return true
+    }
+}
+
+function SetError(element, message){
+    //show error class
+    element.classList.add('invalid')
+    //hide success class
+    element.classList.remove('valid')
+}
+function setSuccess(element, message){
+    //show success class
+    element.classList.add('valid')
+    //hide error class
+    element.classList.remove('invalid')
+}
+
